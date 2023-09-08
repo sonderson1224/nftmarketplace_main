8387eee39f262d566a406b0bafcef06b8e4ed05c
sonderson1224 <realselwyn1@gmail.com>
2023-09-08 06:43:13 +0100

    main

---
+++ nftimgs/index.js
@@ -0,0 +1,146 @@
+import img1 from "./img1.jpg";
+import img2 from "./img2.jpg";
+import img3 from "./img3.jpg";
+import img4 from "./img4.jpg";
+import img5 from "./img5.jpg";
+import img6 from "./img6.jpg";
+import img7 from "./img7.jpg";
+import img8 from "./img8.jpg";
+import img9 from "./img9.jpg";
+import img10 from "./img10.jpg";
+import img11 from "./img11.jpg";
+import img12 from "./img12.jpg";
+import img13 from "./img13.jpg";
+import img14 from "./img14.jpg";
+import img15 from "./img15.jpg";
+import img16 from "./img16.jpg";
+import img17 from "./img17.jpg";
+import img18 from "./img18.jpg";
+import img19 from "./img19.jpg";
+import img20 from "./img20.jpg";
+import img21 from "./img21.jpg";
+import img22 from "./img22.jpg";
+import img23 from "./img23.jpg";
+import img24 from "./img24.jpg";
+import img25 from "./img25.jpg";
+import img26 from "./img26.jpg";
+import img27 from "./img27.jpg";
+import img28 from "./img28.jpg";
+import img29 from "./img29.jpg";
+import img30 from "./img30.jpg";
+import img31 from "./img31.jpg";
+import img32 from "./img32.jpg";
+import img33 from "./img33.jpg";
+import img34 from "./img34.jpg";
+import img35 from "./img35.jpg";
+import img36 from "./img36.jpg";
+import img37 from "./img37.jpg";
+import img38 from "./img38.jpg";
+import img39 from "./img39.jpg";
+import img40 from "./img40.jpg";
+import img41 from "./img41.png";
+import img42 from "./img42.png";
+import img43 from "./img43.png";
+import img44 from "./img44.png";
+import img45 from "./img45.png";
+import img46 from "./img46.png";
+import img47 from "./img47.png";
+import img48 from "./img48.png";
+import img49 from "./img49.png";
+import img50 from "./img50.png";
+import img51 from "./img51.png";
+import img52 from "./img52.png";
+import img53 from "./img53.png";
+import img54 from "./img54.png";
+import img55 from "./img55.png";
+import img56 from "./img56.png";
+import img57 from "./img57.png";
+import img58 from "./img58.png";
+import img59 from "./img59.png";
+import img60 from "./img60.png";
+import img61 from "./img61.png";
+import img62 from "./img62.png";
+import img63 from "./img63.png";
+import img64 from "./img64.png";
+import img65 from "./img65.png";
+import img66 from "./img66.png";
+import img67 from "./img67.png";
+import img68 from "./img68.png";
+import img69 from "./img69.png";
+import img70 from "./img70.png";
+import img71 from "./img71.png";
+
+
+export default {
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12,
+  img13,
+  img14,
+  img15,
+  img16,
+  img17,
+  img18,
+  img19,
+  img20,
+  img21,
+  img22,
+  img23,
+  img24,
+  img25,
+  img26,
+  img27,
+  img28,
+  img29,
+  img30,
+  img31,
+  img32,
+  img33,
+  img34,
+  img35,
+  img36,
+  img37,
+  img38,
+  img39,
+  img40,
+  img41,
+  img42,
+  img43,
+  img44,
+  img45,
+  img46,
+  img47,
+  img48,
+  img49,
+  img50,
+  img51,
+  img52,
+  img53,
+  img54,
+  img55,
+  img56,
+  img57,
+  img58,
+  img59,
+  img60,
+  img61,
+  img62,
+  img63,
+  img64,
+  img65,
+  img66,
+  img67,
+  img68,
+  img69,
+  img70,
+  img71,
+};
