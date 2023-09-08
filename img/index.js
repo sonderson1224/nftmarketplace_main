697bec75332036d26e48913e2543165c0d783e99
sonderson1224 <realselwyn1@gmail.com>
2023-09-08 06:29:14 +0100

    main

---
+++ img/index.js
@@ -0,0 +1,133 @@
+import collection from "./collection.png";
+import creatorbackground1 from "./creatorbackground-1.jpeg";
+import creatorbackground2 from "./creatorbackground-2.jpeg";
+import creatorbackground3 from "./creatorbackground-3.jpeg";
+import creatorbackground4 from "./creatorbackground-4.jpg";
+import creatorbackground5 from "./creatorbackground-5.jpg";
+import creatorbackground6 from "./creatorbackground-6.jpg";
+import creatorbackground7 from "./creatorbackground-7.jpg";
+import creatorbackground8 from "./creatorbackground-8.jpg";
+import creatorbackground9 from "./creatorbackground-9.jpg";
+import creatorbackground10 from "./creatorbackground-10.jpg";
+import creatorbackground11 from "./creatorbackground-11.jpg";
+import user1 from "./user-1.png";
+import user2 from "./user-2.png";
+import user3 from "./user-3.png";
+import user4 from "./user-4.png";
+import user5 from "./user-5.png";
+import user6 from "./user-6.png";
+import user7 from "./user-7.png";
+import user8 from "./user-8.png";
+import user9 from "./user-9.png";
+import user10 from "./user-10.png";
+import service1 from "./service-1.png";
+import service2 from "./service-2.png";
+import service3 from "./service-3.png";
+import service4 from "./service-4.png";
+import logoLight from "./logo-light.svg";
+import logo from "./logo.svg";
+import facebook from "./facebook.svg";
+import twitter from "./twitter.svg";
+import telegram from "./telegram.svg";
+import youtube from "./youtube.svg";
+import vimeo from "./vimeo.svg";
+import vector from "./VectorHIW.svg";
+import zgcmq from "./zgcmq.svg";
+import hero from "./hero.png";
+import nft_1 from "./nft_1.png";
+import getAble from "./getable.png";
+import nft_image_1 from "./nft-image-1.png";
+import nft_image_2 from "./nft-image-2.png";
+import nft_image_3 from "./nft-image-3.png";
+import nft_image_4 from "./nft-image-4.jpeg";
+import nft_image_5 from "./nft-image-5.jpeg";
+import nft_image_6 from "./nft-image-6.jpeg";
+import nft_image_7 from "./nft-image-7.jpeg";
+import nft_image_8 from "./nft-image-8.jpeg";
+import nft_image_9 from "./nft-image-9.jpeg";
+import musiceWave from "./musicwave.png";
+import update from "./update.png";
+import earn from "./earn.png";
+import nftvideo from "./nftvideo.png";
+import upload from "./upload.png";
+import provider_1 from "./provider_1.png";
+import provider_2 from "./provider_2.png";
+import provider_3 from "./provider_3.png";
+import provider_4 from "./provider_4.jpeg";
+import provider_5 from "./provider_5.png";
+import provider_6 from "./provider_6.png";
+import founder_1 from "./founder1.jpg";
+import founder_2 from "./founder2.jpg";
+import founder_3 from "./founder3.jpg";
+import founder_4 from "./founder4.jpg";
+import hero_2 from "./hero2.png";
+import loader from "./loadergif.gif";
+import error from "./error.gif"
+
+export default {
+  hero_2,
+  founder_1,
+  founder_2,
+  founder_3,
+  founder_4,
+  provider_1,
+  provider_2,
+  provider_3,
+  provider_4,
+  provider_5,
+  provider_6,
+  upload,
+  earn,
+  collection,
+  update,
+  musiceWave,
+  nft_image_1,
+  nft_image_2,
+  nft_image_3,
+  nft_image_4,
+  nft_image_5,
+  nft_image_6,
+  nft_image_7,
+  nft_image_8,
+  nft_image_9,
+  nftvideo,
+  getAble,
+  nft_1,
+  hero,
+  error,
+  creatorbackground1,
+  creatorbackground2,
+  creatorbackground3,
+  creatorbackground4,
+  creatorbackground5,
+  creatorbackground6,
+  creatorbackground7,
+  creatorbackground8,
+  creatorbackground9,
+  creatorbackground10,
+  creatorbackground11,
+  user1,
+  user2,
+  user3,
+  user4,
+  user5,
+  user6,
+  user7,
+  user8,
+  user9,
+  user10,
+  service1,
+  service2,
+  service3,
+  service4,
+  logo,
+  logoLight,
+  facebook,
+  telegram,
+  twitter,
+  vector,
+  vimeo,
+  youtube,
+  zgcmq,
+  loader, 
+};
